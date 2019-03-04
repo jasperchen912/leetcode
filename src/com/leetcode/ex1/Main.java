4191c74e487f9888f36bfaa40ce915e636f8c5d6
Jasper <tomato6.chen@outlook.com>
2019-03-04 12:40:48 +0800

    Create basic model.

---
+++ src/com/leetcode/ex1/Main.java
@@ -0,0 +1,8 @@
+package com.leetcode.ex1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/leetcode/ex2/Main.java
@@ -0,0 +1,7 @@
+package com.leetcode.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
